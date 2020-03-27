95498c63e590ddfd8ec9eeb267cf61e680ba050b
abhinav <bhattabhinav@github.com>
2020-03-27 10:20:29 -0400

    search bar added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,41 @@ import React, {Component} from 'react';
 import './App.css';
 import CardList  from './CardList';
 import {robots} from './robots';
+import SearchBox from './SearchBox';
 
 class App extends Component {
 
+  constructor(){
+    
+    super();
+
+    this.state = {
+      robots: robots,
+      searchfield: ''
+    }
+  }
+
+  onSearchChange = (event) => {
+
+    this.setState({
+      searchfield: event.target.value
+    })
+
+    
+  }
+
 
   render(){
+
+
+    const filterdRobots = this.state.robots.filter(robots => {
+      return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    })
   return (
-    <div>
-      <CardList robots = {robots}/>
+    <div className="tc">
+      <h1>Robofriends</h1>
+      <SearchBox searchChange = {this.onSearchChange} />
+      <CardList robots = {filterdRobots}/>
     
   
     </div>
