824f7506e3a0072188bfe83edd75df52d7dfdbf7
abhinav <bhattabhinav@github.com>
2020-03-27 09:24:59 -0400

    cardlist added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Cards  from './Cards';
+import CardList  from './CardList';
 import {robots} from './robots';
 
 class App extends Component {
@@ -10,9 +9,9 @@ class App extends Component {
   render(){
   return (
     <div>
-      <Cards id = {robots[0].id} name = {robots[0].name} email = {robots[0].email}/>
-      <Cards id = {robots[1].id} name = {robots[1].name} email = {robots[1].email}/>
-      <Cards id = {robots[2].id} name = {robots[2].name} email = {robots[2].email}/>
+      <CardList robots = {robots}/>
+    
+  
     </div>
   
   )
---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'tachyons';
+
+const Card = (props) => {
+const {id, name, email} = props;
+    return(
+        <div className="tc bg-light-green dib br3 pa3 ma2 bw2 shadow-5 grow">
+            <img src={"https://robohash.org/" + id + "?200*200"} alt="robots"/>
+            <div>
+                <h1>{name}</h1>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+export default Card;
